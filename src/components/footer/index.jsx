f4e26dc4593ef0799a68f6a9a5870409edab16d8
luarxx <raullennon@raulrockclub.com.br>
2024-04-25 22:00:36 -0300

    Modificação do footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,7 +4,7 @@ import { FaInstagram } from "react-icons/fa";
 
 export function Footer() {
   return (
-    <footer className=" gap-10 p-8 w-full fixed sm:static ">
+    <footer className=" gap-10 p-8 w-full sm:static ">
       <div className="flex justify-center gap-5 ">
         <i className="text-cyan-800 p-2 rounded-2xl border border-zinc-800">
           <FaFacebookF />
